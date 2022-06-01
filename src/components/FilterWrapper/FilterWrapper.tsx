e2277e9371f21698a10720ee2115c74dada1a59f
Mohamed Elattar <mohamed.elattar@cegedim.com>
2022-06-01 02:34:38 +0200

    Refactor to support mobile view

--- src/components/FilterWrapper/FilterWrapper.tsx
+++ src/components/FilterWrapper/FilterWrapper.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import CustomOverlayTrigger from "../CustomOverlayTrigger";
+import clsx from "clsx";
+import { OverlayTrigger, Popover } from "react-bootstrap";
+import "./FilterWrapper.scss";
 
 type Props = {
   title: string;
   content: React.ReactElement;
-  selectedValuesCount: number;
+  selectedValuesCount?: number;
 };
 
-function FilterWrapper({ title, content, selectedValuesCount }: Props) {
+function FilterWrapper({ title, content, selectedValuesCount = 0 }: Props) {
   const [showOverlay, setShowOverlay] = useState(false);
 
   const Component = React.cloneElement(content, {
@@ -15,13 +17,29 @@ function FilterWrapper({ title, content, selectedValuesCount }: Props) {
   });
 
   return (
-    <CustomOverlayTrigger
-      showOverlay={showOverlay}
-      setShowOverlay={setShowOverlay}
-      title={title}
-      selectedValuesCount={selectedValuesCount}
-      content={Component}
-    />
+    <OverlayTrigger
+      trigger="click"
+      show={showOverlay}
+      rootClose
+      placement="bottom"
+      overlay={
+        <Popover id="filters-popover">
+          <Popover.Body>{Component}</Popover.Body>
+        </Popover>
+      }
+    >
+      <button
+        type="button"
+        onClick={() => setShowOverlay(!showOverlay)}
+        className={clsx(
+          "filter",
+          (showOverlay || selectedValuesCount > 0) && "active"
+        )}
+      >
+        <span className="me-1">{title}</span>
+        {selectedValuesCount > 0 && <span>({selectedValuesCount})</span>}
+      </button>
+    </OverlayTrigger>
   );
 }
 
--- src/components/FiltersBar/FiltersBar.tsx
+++ src/components/FiltersBar/FiltersBar.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 import FilterOptionsForm from "../FilterOptionsForm";
-import FilterWrapper from "../FilterWrapper/FilterWrapper";
-import { Filters, FormOptions, FormattedFiltersData } from "../../data-model";
+import FilterWrapper from "../FilterWrapper";
+import { Filters, FormattedFiltersData, FormOptions } from "../../data-model";
+import FiltersAccordion from "../FiltersAccordion";
 
 type Props = {
   selectedValues: Filters;
@@ -26,6 +28,8 @@ function FiltersBar({
     setSelectedFilters(selectedValues);
   }, [selectedValues]);
 
+  const isLargeScreen = useMediaQuery({ query: "(min-width: 650px)" });
+
   const onFilterFormApply = ({ title, checkedOptions }: FormOptions) => {
     if (checkedOptions.length > 0) {
       onSelectFilter({
@@ -39,35 +43,54 @@ function FiltersBar({
     }
   };
 
-  const renderFilterWrappers = (filterForms: React.ReactElement[]) =>
-    filterForms.map((filterForm) => (
+  const renderNavFilters = (filters: FormattedFiltersData) =>
+    filters.map(([title, options]) => (
       <FilterWrapper
-        key={filterForm.props.title}
-        title={filterForm.props.title}
-        content={filterForm}
-        selectedValuesCount={selectedValues[filterForm.props.title]?.length}
-      />
-    ));
-
-  const renderFilterForms = (formattedFiltersData: FormattedFiltersData) =>
-    formattedFiltersData.map(([title, options]) => (
-      <FilterOptionsForm
+        key={title}
         title={title}
-        options={options}
-        onApply={onFilterFormApply}
-        selectedValues={selectedValues[title]}
+        selectedValuesCount={selectedValues[title]?.length}
+        content={
+          <FilterOptionsForm
+            title={title}
+            options={options}
+            selectedValues={selectedValues[title]}
+            onApply={onFilterFormApply}
+          />
+        }
       />
     ));
 
-  const renderAllFilters = () => {
-    const formattedFiltersData: FormattedFiltersData =
-      Object.entries(filtersData);
+  const renderCollapsibleFilters = (filters: FormattedFiltersData) => (
+    <FilterWrapper
+      key="more-filters"
+      title="More Filters"
+      content={
+        <FiltersAccordion
+          filters={filters}
+          selectedValues={selectedValues}
+          onApply={onFilterFormApply}
+        />
+      }
+    />
+  );
+
+  const renderLargeScreenFilters = () =>
+    renderNavFilters(Object.entries(filtersData));
+
+  const renderMobileFilters = () => {
+    const formattedFiltersData = Object.entries(filtersData);
     const filters = [];
-    const filterForms = renderFilterForms(formattedFiltersData);
-    const filterWrappers = renderFilterWrappers(filterForms);
-    filters.push(filterWrappers);
+    const navFilters = renderNavFilters(formattedFiltersData.slice(0, 2));
+    filters.push(navFilters);
+    const collapsibleFilters = renderCollapsibleFilters(
+      formattedFiltersData.slice(2)
+    );
+    filters.push(collapsibleFilters);
     return filters;
   };
+
+  const renderAllFilters = () =>
+    isLargeScreen ? renderLargeScreenFilters() : renderMobileFilters();
 
   return <>{renderAllFilters()}</>;
 }
